6b00c0144add7d5a587363e456528c4eb5c1748f
Kissaritari <97673974+Kissaritari@users.noreply.github.com>
2023-02-02 23:39:45 +0200

    yritetty on

--- Pikipedia/Assets/Scripts/healthBar.cs
+++ Pikipedia/Assets/Scripts/healthBar.cs
@@ -7,14 +7,20 @@ public class HealthBar : MonoBehaviour
 {
     public Slider healthBar;
     public Health playerHealth;
+
      private void Start()
     {
         playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
         healthBar = GetComponent<Slider>();
         healthBar.maxValue = playerHealth.maxHealth;
         healthBar.value = playerHealth.maxHealth;
+   
     }
 
+    public void update()
+    {
+       // transform.position.z = 0;
+    }
     public void SetHealth(int hp)
     {
         healthBar.value = hp;
