33c0127eac122c8c8d7c0839f5605345129c850c
Kissaritari <97673974+Kissaritari@users.noreply.github.com>
2023-02-08 19:35:33 +0200

    health bar kääntyy niinku pitää

--- Pikipedia/Assets/Scripts/healthBar.cs
+++ Pikipedia/Assets/Scripts/healthBar.cs
@@ -7,22 +7,26 @@ public class HealthBar : MonoBehaviour
 {
     public Slider healthBar;
     public Health playerHealth;
-     
-    public PlayerMovement playerMovement;
     
+    public Transform TargetToFollow;
+    private Vector3 Offset;
      private void Start()
     {
         playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
         healthBar = GetComponent<Slider>();
         healthBar.maxValue = playerHealth.maxHealth;
         healthBar.value = playerHealth.maxHealth;
-   
+        
+        Offset = transform.position - TargetToFollow.position;	
     }
 
     public void update()
     {
-       // transform.position.z = 0;
     }
+    void LateUpdate ()
+	{
+		transform.position = TargetToFollow.position + Offset;	
+	}
     public void SetHealth(int hp)
     {
         healthBar.value = hp;
