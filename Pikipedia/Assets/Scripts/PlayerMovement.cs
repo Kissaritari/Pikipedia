481b7e34b00f6282296cd13e694af22ad1d48cf0
GachiPls <antti.puoskari@seamk.fi>
2023-02-13 12:16:03 +0200

    scuffed ground check ja sound effect

--- Pikipedia/Assets/Scripts/PlayerMovement.cs
+++ Pikipedia/Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,19 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
+
+    [SerializeField] private LayerMask JumpableGround;
 
     [SerializeField] private AudioSource Jump;
+    [SerializeField] private AudioSource Walk;
+
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -21,10 +27,24 @@ public class PlayerMovement : MonoBehaviour
         float dirX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+       if(rb.velocity.magnitude > 2f && IsGrounded())
+        {
+            Walk.enabled = true;
+        }
+        else
+        {
+            Walk.enabled = false;
+        }
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, 13f);
             Jump.Play();
         }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, JumpableGround);
     }
 }
