e2fab36ecfffd2c17a57f1808c4538ad539bd293
Kissaritari <97673974+Kissaritari@users.noreply.github.com>
2023-02-20 00:23:01 +0200

    oisko nyt

--- Pikipedia/Assets/Scripts/Bullet.cs
+++ Pikipedia/Assets/Scripts/Bullet.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-<<<<<<< Updated upstream
     public float speed = 20f;
     public int damage = 20;
-=======
-
-    public float speed = 5f;
->>>>>>> Stashed changes
     public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
@@ -18,7 +13,6 @@ public class Bullet : MonoBehaviour
         rb.velocity = transform.right * speed;
     }
 
-<<<<<<< Updated upstream
     void OnTriggerEnter2D(Collider2D hitInfo) 
     {
         Player player = hitInfo.GetComponent<Player>();
@@ -30,17 +24,4 @@ public class Bullet : MonoBehaviour
         Destroy(gameObject);
     }
 
-=======
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void OnTriggerEnter2D (Collider2D hitInfo)
-    {
-        Debug.Log(hitInfo.name);
-        Destroy(gameObject);
-    }
->>>>>>> Stashed changes
 }
