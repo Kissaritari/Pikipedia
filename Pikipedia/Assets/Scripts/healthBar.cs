654438d6be3b18756374c25ab5c9f8f2423ef085
Kissaritari <97673974+Kissaritari@users.noreply.github.com>
2023-03-16 13:54:35 +0200

    Update healthBar.cs

--- Pikipedia/Assets/Scripts/healthBar.cs
+++ Pikipedia/Assets/Scripts/healthBar.cs
@@ -6,31 +6,28 @@ using UnityEngine.UI;
 public class healthBar : MonoBehaviour
 {
     private Slider SliderHealthBar;
-    public Player player;
+    public Player player; // the player object whose health is tracked
     
-    public Transform TargetToFollow;
+    public Transform TargetToFollow; // The selected target (player) the healthbar follows
     private Vector3 Offset;
      private void Start()
     {
-        SliderHealthBar = GetComponent<Slider>();
+        SliderHealthBar = GetComponent<Slider>();   // selects the slider component for the healthbar to use
         SliderHealthBar.maxValue = player.health;
-        SliderHealthBar.value = player.health;
+        SliderHealthBar.value = player.health;  // sets the max and current values to the healthbar
         
-        Offset = transform.position - TargetToFollow.position;	
+        Offset = transform.position - TargetToFollow.position;	// sets the starting position of the healthbar
     }
 
-    public void update()
-    {
-    }
     void LateUpdate ()
 	{
-		transform.position = TargetToFollow.position + Offset;	
+		transform.position = TargetToFollow.position + Offset;  // moves the healthbar according to the movement of the follow target 
 	}
-    public void SetHealth(int hp)
+    public void SetHealth(int HitPoints)
     {
-        SliderHealthBar.value = hp;
+        SliderHealthBar.value = HitPoints;
         if ( SliderHealthBar.value <= 0 ) {
-            Destroy(gameObject);
+            Destroy(gameObject);                // if the value of the healthbar drops down to zero or less, destroys the healthbar object
         }
     }
 }
