7bdd9a0f349bc4d4517d2bcbe5b6ae47a49573a9
Shrimpage <jphaapaniemi@gmail.com>
2023-04-13 17:13:27 +0300

    Update Powerup.cs
    jatkoa

--- Pikipedia/Assets/Scripts/Powerup.cs
+++ Pikipedia/Assets/Scripts/Powerup.cs
@@ -6,26 +6,30 @@ public class Powerup : MonoBehaviour
 {
     public GameObject pickupEffect;
     public float duration = 5f;
+	private int damage = -20;
 
    void OnTriggerEnter2D (Collider2D collision) 
-   {
+	{
         
         if (collision.tag == "Player")
         {
             StartCoroutine( Pickup(collision) );            
         }
-   }
+	}
 
-   IEnumerator Pickup(Collider2D player)
-   {
-        Instantiate(pickupEffect, transform.position, transform.rotation);
+    IEnumerator Pickup(Collider2D player)
+	{
+		Instantiate(pickupEffect, transform.position, transform.rotation);
+
+		Player play = player.GetComponent<Player>();
+		play.TakeDamage(damage);
 
         GetComponent<SpriteRenderer>().enabled = false;
-        GetComponent<Rigidbody2D>().simulated = false;
+		GetComponent<Rigidbody2D>().simulated = false;
         GetComponent<Collider2D>().enabled = false;
 
-        yield return new WaitForSeconds(duration);
+    	yield return new WaitForSeconds(duration);
 
-        Destroy(gameObject);
-   }
+		Destroy(gameObject);
+	}
 }
